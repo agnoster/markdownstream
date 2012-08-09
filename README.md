86a5c93cde475497a0e82a3958425226a00acfd0
Isaac Wolkerstorfer <agnoster@gmail.com>
2012-08-09 15:01:57 +0200

    Allow re-rendering code chunks (e.g. to fenced version)
    Fixes #3

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # MarkDownStream [![build status](https://secure.travis-ci.org/agnoster/markdownstream.png)](http://travis-ci.org/agnoster/markdownstream)
 
-Streaming markdown parser that allows round-tripping and patching
+Streaming markdown parser that will allows round-tripping and patching
 
 Ever wanted to parse a markdown file, and *just* fix certain things? Like maybe extract all the code segments, or add numbering to the headers, or fix all the numberings of ordered lists?
 
 Well, now you can.
 
-    var MarkdownStream = require('markdownstream')
-    parse = new MarkdownStream
-    ... more to come ...
-
 ...
 
 Actually, right *now* you can't, because I'm still working on it.
 
-## Goals for 0.1:
+## What you can do right now:
+
+* Stream through a file
+* Capture all code blocks, both in raw and parsed form
+
+## Goals for 0.0.1:
 
 * Read code blocks
 * Round-trip a file, manipulating *only* the code blocks
--- test/index.js
+++ test/index.js
@@ -32,17 +32,18 @@ test("can emit events for a code block", function(t) {
       , fcode = []
 
     input.pipe(parser)
-    parser.on('data', function(data, metadata) {
+    parser.on('data', function(chunk) {
         
-        buffer += data
-        if (metadata.token == 'code_block' && !metadata.fenced) {
-            code.push(data)
-            codeparsed.push(metadata.content)
+        buffer += chunk
+        if (chunk.type == 'code_block' && !chunk.fenced) {
+            code.push(chunk.toString())
+            codeparsed.push(chunk.content)
         }
-        if (metadata.token == 'code_block' && metadata.fenced) {
-            fcode.push(metadata)
+        if (chunk.type == 'code_block' && chunk.fenced) {
+            fcode.push(chunk.toString())
+            codeparsed.push(chunk.content)
         }
-        output.write(data)
+        output.write(chunk)
     })
 
     parser.on('end', function() {
@@ -51,11 +52,37 @@ test("can emit events for a code block", function(t) {
         t.equal(buffer, doc, "Buffer should be unchanged")
         t.equal(output.output, doc, "Output should be unchanged")
         t.deepEqual(code, ["    IC1\n    IC1\n\n    IC1\n", "    \n    IC2\n    \n"])
-        t.deepEqual(codeparsed, ["IC1\nIC1\n\nIC1\n", "\nIC2\n\n"])
-        t.deepEqual(fcode, [{ token: 'code_block', content: 'FC1\n\nFC1\n\n', tags: '', fenced: true }
-                       ,{ token: 'code_block', content: 'FC2\nFC2\n\n    FC2\n', tags: 'foo', fenced: true }])
+        t.deepEqual(fcode, ["```\nFC1\n\nFC1\n\n```\n", "```foo\nFC2\nFC2\n\n    FC2\n```\n"])
+        t.deepEqual(codeparsed, ["IC1\nIC1\n\nIC1\n", "\nIC2\n\n", "FC1\n\nFC1\n\n", "FC2\nFC2\n\n    FC2\n"])
         t.end()
     })
 
     input.resume()
+})
+
+test("can re-write code blocks", function(t) {
+
+    var parser = new MarkdownStream
+      , output = ''
+
+    parser.on('data', function(chunk) {
+
+        if (chunk.type == 'code_block') {
+            
+            chunk.tags = 'foo'
+            output += chunk.render()
+        } else {
+
+            output += chunk.toString()
+        }
+    })
+
+    parser.on('end', function() {
+
+        t.equal(output, "# Hello\n\n```foo\nfoobar\ntest\n```\n\nThat's that!\n")
+        t.end()
+    })
+    
+    parser.write("# Hello\n\n    foobar\n    test\n\nThat's that!\n")
+    parser.end()
 })
